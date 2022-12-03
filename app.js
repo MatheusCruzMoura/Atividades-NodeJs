fd53efaca06b15feaf465245ac2651bcc38b2901
Matheus Cruz de Moura <matheuscruz.moura27@gmail.com>
2022-12-03 19:44:24 -0300

    criando os containers do banco de dados e conectando com a API

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 const express = require('express');
 require('dotenv').config();
 
+const mysql = require('mysql2');
+const connect = require('./conexao.js');
+
 // Creating express object
 const app = express();
 
@@ -14,6 +17,12 @@ app.get('/', (req, res) => {
 app.get('/node', (req, res) => {
     res.send('Aplicação rodando no node v18.12.1')
     res.end()
+})
+
+app.get('/clientes', (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    return connect.execSQLQuery('select * from cliente', res);
 })
 
 // Port Number
