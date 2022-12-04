f665f8f28b7d0e4321e3ff136ff27e5a19aaa48f
Matheus Cruz de Moura <matheuscruz.moura27@gmail.com>
2022-12-04 17:27:52 -0300

    primeira pagina com react native

--- mobile/App.js
+++ mobile/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.texto}>Bem vindo ao React Native!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#222a34',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  texto: {
+    color: '#fff',
+    fontSize: '1.5rem'
+  }
 });
