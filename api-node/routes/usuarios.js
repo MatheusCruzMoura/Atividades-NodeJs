51c6a438c924b66654e644cfaecd3f3a53fd5d22
Matheus Cruz de Moura <matheuscruz.moura27@gmail.com>
2022-12-10 19:47:47 -0300

    validando login

--- api-node/routes/usuarios.js
+++ api-node/routes/usuarios.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
+var jwt = require('jsonwebtoken');
 
-// const mysql = require('mysql2');
+const mysql = require('mysql2');
+require('dotenv').config();
+
 const connect = require('../conexao.js');
+
+const connection = mysql.createConnection({
+    host: process.env.BANCO_HOST,
+    port: process.env.BANCO_PORT,
+    user: process.env.BANCO_USER,
+    password: process.env.BANCO_SENHA,
+    database: process.env.BANCO
+});
 
 router.post('/usuarios', (req, res) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
@@ -13,11 +24,33 @@ router.post('/usuarios', (req, res) => {
     if (hash != undefined) {
         return connect.execSQLQuery("insert into usuario (email, senha) value('" + req.body.email + "', '" + hash + "')", res);
     }
-    // res.send(req.body.email + ", " + hash)
-    // res.end()
-    // req.send(req.body.email, hash)
-    // req.end()
-    // return connect.execSQLQuery(`insert into usuario (email, senha) value(${req.body.email}, ${hash})`, res);
+})
+
+router.post('/usuarios/login', (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'POST');
+
+    connection.query(
+        'SELECT senha FROM usuario WHERE email = ?',
+        [req.body.email],
+        function (err, results) {
+            if (results.length > 0) {
+                bcrypt.compare(req.body.senha, results[0].senha, function (err, result) {
+                    if (result) {
+                        var token = jwt.sign({ email: req.body.email }, process.env.PRIVATE_KEY, { expiresIn: 60 / 2 });
+                        res.json({ "token": token })
+                        res.end()
+                    } else {
+                        res.json({ "token": undefined })
+                        res.end()
+                    }
+                })
+            } else {
+                res.json({ "token": undefined })
+                res.end()
+            };
+        }
+    );
 })
 
 module.exports = router;
