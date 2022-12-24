b692445a55be689963eba33cb59bf7f9b1481a17
Matheus Cruz de Moura <matheuscruz.moura27@gmail.com>
2022-12-24 07:51:33 -0300

    tela de login com bootstrap

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,17 +1,16 @@
-import { useState } from 'react'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <div className="container">
+      <div className="caixa">
         <section className="login-box">
           <img className="banner" src="https://bit.ly/3vcjmXV" alt="" />
 
           <form className="login-form">
-            <h1>Login</h1>
+            <h1>Login HTML</h1>
 
             <div className="login-input-box">
               <label htmlFor="email">E-mail</label>
@@ -26,6 +25,25 @@ function App() {
             <button type="submit">Entrar</button>
           </form>
         </section>
+      </div>
+
+      <div className="caixa2">
+        <div className="login-box2">
+          <img className="banner2" src="https://bit.ly/3vcjmXV" alt="" />
+
+          <Form className="login-form2">
+            <h1>Login Bootstrap</h1>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>E-mail</Form.Label>
+              <Form.Control type="email" placeholder="e-mail" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Senha</Form.Label>
+              <Form.Control type="password" placeholder="senha" />
+            </Form.Group>
+            <Button className="login-submit" variant="primary" type="submit">Entrar</Button>
+          </Form>
+        </div>
       </div>
     </div>
   )
